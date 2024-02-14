adb97cbaf5dc171023258465ed18ea0b80bb7e35
Keli06001 <keli06001@utb.vaxjo.se>
2024-02-14 10:37:52 +0100

    Add project files.

---
+++ ILU6.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ILU6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa=Addera(45, 45, 10);
+            Console.WriteLine(summa);
+
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            return tal1+tal2+tal3;
+        }
+
+
+    }
+}
